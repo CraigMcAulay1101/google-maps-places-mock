120f59881d3df73c9caa6adecca2542aa37edc0f
Craig McAulay <craigmcaulay98@googlemail.com>
2019-03-08 23:50:09 +0000

    Google Maps Places invoker implemented

--- src/GooglePlacesMock.js
+++ src/GooglePlacesMock.js
@@ -1,14 +1,27 @@
 "use strict";
 
-const GooglePlacesMock = {
+const constructPlace = require('./construct.js');
 
-    Mock: (params) => {
-        if (typeof variable !== 'undefined') {
+class GooglePlacesMock {
 
+    constructor(location, radius, results, type) {
+
+        if (location === undefined || radius === undefined) {
+            throw new Error("Missing location or radius parameter");
         } else {
-            
+            this.location = location;
+            this.radius = radius;
+            this.results = results || 20;
+            this.type = type || "random";
         }
-    } 
+    }
+
+    nearbySearch() {
+        constructPlace(this).then(() => {
+
+        });
+    }
+
 }
 
 module.exports = GooglePlacesMock; 
---
+++ src/construct.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const schema = require('./schema.json');
+
+const constructPlace = new Promise ((resolve, reject) => {
+
+});
+
+module.exports = constructPlace;
