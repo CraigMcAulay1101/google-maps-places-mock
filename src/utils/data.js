4da4c3fc1824020ebdd067ba790ff811329f2688
Craig McAulay <craigmcaulay98@googlemail.com>
2019-03-10 13:29:32 +0000

    functionality to generate dummy data

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const loremIpsum = require('lorem-ipsum');
+
+const generateData = ((params) => {
+    
+    Object.keys(params).forEach((key) => {
+        if (typeof params[key] === 'object') {
+            generateData(params[key]);
+        } else {
+            let data = validateData(params[key]);
+            params[key] = data;
+        }
+    })
+
+    return params;
+});
+
+function validateData(data) {
+    if (data === 'Number') {
+        return generateNumber();
+    } else if (data === 'String') {
+        return generateString();
+    }
+}
+
+function generateNumber() {
+    let base = Math.floor(Math.random() * 20);
+    let multiplier = Math.floor(Math.random() * 20);
+
+    return base * multiplier * 50000;
+}
+
+function generateString() {
+    return loremIpsum();
+}
+
+module.exports = generateData;  
