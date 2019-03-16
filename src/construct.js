ea6ea10160e1489e9a33ec8f259b9e654ccc37ad
Craig McAulay <craigmcaulay98@googlemail.com>
2019-03-16 16:14:11 +0000

    Coerce Schema

--- src/construct.js
+++ src/construct.js
@@ -1,11 +1,33 @@
 "use strict";
 
-const schema = require('./schema.js');
+const schm = require('schm')
+const getSchema = require('./schema.js');
+const generateData = require('./utils/data');
 
 let constructPlace = ((params) => {
     return new Promise((resolve, reject) => {
-        resolve();
+ 
+        getSchema.then((schema) => {
+            let coerce = new coerceSchema(schema);
+            let place = coerce.coerceSchema();
+
+            resolve(place);            
+        });
+
     });
 });
+
+class coerceSchema {
+    constructor(schema) {
+        this.schema = schema
+    }
+
+    coerceSchema() {
+        let generatedSchema = schm(this.schema);
+        let dummydata = generateData(this.schema);
+    
+        return generatedSchema.parse(dummydata);    
+    }
+}
 
 module.exports = constructPlace;
