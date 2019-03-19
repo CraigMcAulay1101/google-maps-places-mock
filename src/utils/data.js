f1a01d0d0e9b7ad1a364b34188091658c8addaa1
Craig McAulay <craigmcaulay98@googlemail.com>
2019-03-19 23:13:58 +0000

    Improve bearings a bit

--- src/utils/data.js
+++ src/utils/data.js
@@ -20,10 +20,15 @@ const generateData = ((params) => {
 
 const generateLocation = ((geoInfo) => {
     let initialPoint = { lat: geoInfo.location.lat, lon: geoInfo.location.lng };
-    let rndm = Math.floor(Math.random() * 999) + 1;
-    let radius = geoInfo.radius + rndm;
+
+    let rndm = Math.floor(Math.random() * 999);
     
-    return geolib.computeDestinationPoint(initialPoint, radius, 45);
+    let radius = geoInfo.radius + rndm;
+
+    let bearings = [22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 280, 302.5, 325, 347.5, 360];
+    let bearing = bearings[Math.floor(Math.random()*bearings.length)];
+    
+    return geolib.computeDestinationPoint(initialPoint, radius, bearings);
 })
 
 class Dummy {
